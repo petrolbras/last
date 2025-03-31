cc20113dd3e6b3d9796ea91ffedc6c9f8374b01c
Petrolbras <pietrogouvearepleop@gmail.com>
2025-03-31 14:31:29 -0300

    qweqewq

---
+++ last.js
@@ -0,0 +1,7 @@
+let last = document.getElementById('lastruim')
+let vros = document.getElementById('vro')
+
+for (let i = 0; i < 500; i++){
+    last.innerHTML += "last ruim"
+    vros.innerHTML += "Vro?"
+}
